6ac464cea3b81be27fa88b278ed33672df5ed02a
Vandeth <vandeth.ifl@gmail.com>
2024-11-27 10:56:06 -0600

    refactor code for prog4

--- src/HomeWork01/Prog4.java
+++ src/HomeWork01/Prog4.java
@@ -3,17 +3,41 @@ package HomeWork01;
 import java.util.Arrays;
 
 public class Prog4 {
-
     public static void main(String[] args) {
-        String[] animals =  {"horse", "dog", "cat", "horse", "dog"};
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String animal : animals) {
-            if(stringBuilder.indexOf(animal) == -1) {
-                stringBuilder.append(animal).append(",");
+        String[] animal = {"horse", "dog", "cat", "horse", "dog","do","hors" };
+        String[] NoDuplicate = new String[animal.length];
+        int index = 0;
+        for (String s : animal)
+        {
+            boolean found = false;
+            for (int j = 0; j < index; j++)
+            {
+                if (s.equals(NoDuplicate[j]))
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                NoDuplicate[index] = s;
+                index += 1;
             }
         }
-        String[] removedDuplicates = stringBuilder.toString().split(",");
-        System.out.println(Arrays.toString(removedDuplicates));
+        int count = 0;
+        for (String s : NoDuplicate) {
+            if (s != null){
+               count++;
+            }
+        }
+        System.out.println(Arrays.toString(animal));
+        System.out.println(Arrays.toString(NoDuplicate));
+        String[] result = new String[count];
+        for (int i = 0; i <count ; i++) {
+            result[i] = NoDuplicate[i];
+        }
+        System.out.print(Arrays.toString(result));
 
     }
 }
+
--- src/HomeWork01/Prog6.java
+++ src/HomeWork01/Prog6.java
@@ -3,10 +3,9 @@ package HomeWork01;
 public class Prog6 {
 
     public static void main(String[] args) {
-        int[] arrayOfInts = {2, 21, 3, 45, 0, 12, 18, 6, 3, 1, 0, 22};
+        int[] arrayOfInts = {2,21,3,45,12,18,6,-3,-1,22};
         secondMin(arrayOfInts);
     }
-
     public static void secondMin(int[] arrayOfInts){
         int minValue = 2147483647;
         int secondMinValue = 2147483647;
